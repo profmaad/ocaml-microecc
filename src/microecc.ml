6a7d134985af9d53f8d98e784272934baa07de20
Prof. MAAD <profmaad@users.noreply.github.com>
2016-09-03 11:47:10 +0800

    Update micro-ecc, adapt to API changes

--- src/microecc.ml
+++ src/microecc.ml
@@ -1,13 +1,25 @@
 open Ctypes;;
 open Foreign;;
 
-let bytes = 32;;
-let public_key_length = 2 * bytes;;
-let private_key_length = bytes;;
-let shared_secret_length = bytes;;
-let compressed_point_length = bytes + 1;;
-let hash_length = bytes;;
-let signature_length = 2 * bytes;;
+module Curve = struct
+  type t
+  let t : t structure typ = structure "uECC_Curve_t"
+
+  let private_key_size = foreign "uECC_curve_private_key_size" (t @-> returning int)
+  let public_key_size  = foreign "uECC_curve_public_key_size"  (t @-> returning int)
+
+  let curve_size t = (public_key_size t) / 2
+
+  let compressed_point_size t = curve_size t + 1
+  let shared_secret_size      = curve_size
+  let signature_size          = public_key_size
+
+  let secp160r1 = foreign "uECC_secp160r1" (void @-> returning t)
+  let secp192r1 = foreign "uECC_secp192r1" (void @-> returning t)
+  let secp224r1 = foreign "uECC_secp224r1" (void @-> returning t)
+  let secp256r1 = foreign "uECC_secp256r1" (void @-> returning t)
+  let secp256k1 = foreign "uECC_secp256k1" (void @-> returning t)
+end
 
 let char_array_of_string s =
   let length = String.length s in
@@ -24,64 +36,81 @@ let string_of_uint8_array a length =
     ~length:length
 ;;
 
-let make_key_c = foreign "uECC_make_key" (ptr uint8_t @-> ptr uint8_t @-> returning int);;
-let make_key () = 
-  let public_key_array = CArray.make uint8_t public_key_length in
-  let private_key_array = CArray.make uint8_t private_key_length in
-  let result = make_key_c (CArray.start public_key_array) (CArray.start private_key_array) in
-  let public_key = string_of_uint8_array public_key_array public_key_length in
-  let private_key = string_of_uint8_array private_key_array private_key_length in
+let make_key_c = foreign "uECC_make_key" (ptr uint8_t @-> ptr uint8_t @-> Curve.t @-> returning int);;
+let make_key curve =
+  let public_key_array  = CArray.make uint8_t (Curve.public_key_size curve)  in
+  let private_key_array = CArray.make uint8_t (Curve.private_key_size curve) in
+  let result = make_key_c (CArray.start public_key_array) (CArray.start private_key_array) curve in
+  let public_key  = string_of_uint8_array public_key_array  (Curve.public_key_size curve)  in
+  let private_key = string_of_uint8_array private_key_array (Curve.private_key_size curve) in
   match result with
   | 1 -> Some (public_key, private_key)
   | _ -> None
 ;;
 
-let shared_secret_c = foreign "uECC_shared_secret" (ptr uint8_t @-> ptr uint8_t @-> ptr uint8_t @-> returning int);;
-let shared_secret opposite_public_key private_key =
+let shared_secret_c = foreign "uECC_shared_secret" (ptr uint8_t @-> ptr uint8_t @-> ptr uint8_t @-> Curve.t @-> returning int);;
+let shared_secret curve opposite_public_key private_key =
   let public_key_array = char_array_of_string opposite_public_key in
   let private_key_array = char_array_of_string private_key in
-  let shared_secret_array = CArray.make uint8_t shared_secret_length in
-  let result = shared_secret_c (uint8_ptr_of_char_array public_key_array) (uint8_ptr_of_char_array private_key_array) (CArray.start shared_secret_array) in
-  let shared_secret_string = string_of_uint8_array shared_secret_array shared_secret_length in
+  let shared_secret_array = CArray.make uint8_t (Curve.shared_secret_size curve) in
+  let result = shared_secret_c (uint8_ptr_of_char_array public_key_array) (uint8_ptr_of_char_array private_key_array) (CArray.start shared_secret_array) curve in
+  let shared_secret_string = string_of_uint8_array shared_secret_array (Curve.shared_secret_size curve) in
   match result with
   | 1 -> Some shared_secret_string
   | _ -> None
 ;;
 
-let compress_c = foreign "uECC_compress" (ptr uint8_t @-> ptr uint8_t @-> returning void);;
-let compress public_key =
+let compress_c = foreign "uECC_compress" (ptr uint8_t @-> ptr uint8_t @-> Curve.t @-> returning void);;
+let compress curve public_key =
   let public_key_array = char_array_of_string public_key in
-  let compressed_point_array = CArray.make uint8_t compressed_point_length in
-  compress_c (uint8_ptr_of_char_array public_key_array) (CArray.start compressed_point_array);
-  string_of_uint8_array compressed_point_array compressed_point_length
+  let compressed_point_array = CArray.make uint8_t (Curve.compressed_point_size curve) in
+  compress_c (uint8_ptr_of_char_array public_key_array) (CArray.start compressed_point_array) curve;
+  string_of_uint8_array compressed_point_array (Curve.compressed_point_size curve)
 ;;
 
-let decompress_c = foreign "uECC_decompress" (ptr uint8_t @-> ptr uint8_t @-> returning void);;
-let decompress compressed_point =
+let decompress_c = foreign "uECC_decompress" (ptr uint8_t @-> ptr uint8_t @-> Curve.t @-> returning void);;
+let decompress curve compressed_point =
   let compressed_point_array = char_array_of_string compressed_point in
-  let public_key_array = CArray.make uint8_t public_key_length in
-  decompress_c (uint8_ptr_of_char_array compressed_point_array) (CArray.start public_key_array);
-  string_of_uint8_array public_key_array public_key_length
+  let public_key_array = CArray.make uint8_t (Curve.public_key_size curve) in
+  decompress_c (uint8_ptr_of_char_array compressed_point_array) (CArray.start public_key_array) curve;
+  string_of_uint8_array public_key_array (Curve.public_key_size curve)
 ;;
 
-let sign_c = foreign "uECC_sign" (ptr uint8_t @-> ptr uint8_t @-> ptr uint8_t @-> returning int);;
-let sign private_key hash =
+let sign_c = foreign "uECC_sign" (ptr uint8_t @-> ptr uint8_t @-> uint64_t @-> ptr uint8_t @-> Curve.t @-> returning int);;
+let sign curve private_key hash =
   let private_key_array = char_array_of_string private_key in
   let hash_array = char_array_of_string hash in
-  let signature_array = CArray.make uint8_t signature_length in
-  let result = sign_c (uint8_ptr_of_char_array private_key_array) (uint8_ptr_of_char_array hash_array) (CArray.start signature_array) in
-  let signature = string_of_uint8_array signature_array signature_length in
+  let hash_size  = String.length hash in
+  let signature_array = CArray.make uint8_t (Curve.signature_size curve) in
+  let result =
+    sign_c
+      (uint8_ptr_of_char_array private_key_array)
+      (uint8_ptr_of_char_array hash_array)
+      hash_size
+      (CArray.start signature_array)
+      curve
+  in
+  let signature = string_of_uint8_array signature_array (Curve.signature_size curve) in
   match result with
   | 1 -> Some signature
   | _ -> None
 ;;
 
-let verify_c = foreign "uECC_verify" (ptr uint8_t @-> ptr uint8_t @-> ptr uint8_t @-> returning int);;
-let verify public_key hash signature =
+let verify_c = foreign "uECC_verify" (ptr uint8_t @-> ptr uint8_t @-> uint64t @-> ptr uint8_t @-> Curve.t @-> returning int);;
+let verify curve public_key hash signature =
   let public_key_array = char_array_of_string public_key in
   let hash_array = char_array_of_string hash in
+  let hash_size = String.length hash in
   let signature_array = char_array_of_string signature in
-  match verify_c (uint8_ptr_of_char_array public_key_array) (uint8_ptr_of_char_array hash_array) (uint8_ptr_of_char_array signature_array) with
+  let result =
+    verify_c
+      (uint8_ptr_of_char_array public_key_array)
+      (uint8_ptr_of_char_array hash_array)
+      hash_size
+      (uint8_ptr_of_char_array signature_array)
+      curve
+  in
+  match result with
   | 1 -> true
   | _ -> false
 ;;
--- src/microecc.mli
+++ src/microecc.mli
@@ -1,29 +1,42 @@
 (** OCaml bindings for the micro-ecc library, implementing ECDH and ECDSA.
     All vlaues in this module are handled as binary strings. *)
 
+module Curve : sig
+  type t
+
+  val secp160r1 : t
+  val secp192r1 : t
+  val secp224r1 : t
+  val secp256r1 : t
+  val secp256k1 : t
+
+  val private_key_size : t -> int
+  val public_key_size  : t -> int
+end
+
 (** Generates a new ECC keypair, if successful.
     @return If successful, returns Some (public_key, private_key). None otherwise *)
-val make_key : unit -> (string * string) option
+val make_key : Curve.t -> (string * string) option
 
 (** Generates the ECDH shared secret given the other parties public key and our private key
     @return If successful, returns Some shared_secret, None otherwise. *)
-val shared_secret : string -> string -> string option
+val shared_secret : Curve.t -> string -> string -> string option
 
 (** Compresses the given public key into the compressed point representation
     @return compressed point representation of the given public key *)
-val compress : string -> string
+val compress : Curve.t -> string -> string
 
 (** Decompresses a public key in compressed point form
     @return the decompressed public key *)
-val decompress : string -> string
+val decompress : Curve.t -> string -> string
 
 (** Given a private key and a hash, it signs the hash and returns the signature.
     Signatures are given as two points, r and s, concatenated into one string.
     Please note that this does not return DER representation as used by OpenSSL.
     @return Some signature if successful, None otherwise *)
-val sign : string -> string -> string option
+val sign : Curve.t -> string -> string -> string option
 
 (** Given a public key, a hash and a signature, it verifies the signature against the hash and public key.
     The signature is expected to be in the same format returned by sign, not in DER format.
     @return true if the signature was verified successfully, false otherwise. *)
-val verify : string -> string -> string -> bool
+val verify : Curve.t -> string -> string -> string -> bool
